4a5319b0f6cfe3cfcb10166168657c8f0296e071
Dmitry221060 <ya.dima42103ya@yandex.ru>
2025-09-04 03:42:21 +0500

    Remove old project files

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import config from "./config";
-import server from "./server";
-import logger from "./utils/logger";
-
-const { port } = config.server;
-server.listen(port, () => logger.info(`Server running at port ${port}`));
--- src/server/routes.ts
+++
@@ -1,8 +0,0 @@
-import { Application } from "express";
-import leaderboardController from "../controllers/leaderboard.controller";
-import scoreController from "../controllers/score.controller";
-
-export default (app: Application): void => {
-  app.use(leaderboardController);
-  app.use(scoreController);
-};
