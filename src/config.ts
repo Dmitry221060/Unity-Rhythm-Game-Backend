520a857995749497ccbe9420d14940b225bcdb1f
Dmitry221060 <ya.dima42103ya@yandex.ru>
2025-09-04 03:45:46 +0500

    Update configs for TypeORM

--- src/config.ts
+++ src/config.ts
@@ -1,26 +1,33 @@
-/* eslint-disable @typescript-eslint/no-magic-numbers */
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 import dotenv from "dotenv";
-dotenv.config()
+dotenv.config();
 
 const config = {
   server: {
-    port: process.env.PORT ?? 8080
+    port: process.env.PORT ?? 8080,
   },
 
-  db: {
-    url: process.env.MONGO_URL
+  getTypeOrmConfig: (): TypeOrmModuleOptions => {
+    return {
+      type: "mongodb",
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT ?? "27017"),
+      database: process.env.DB_NAME,
+      synchronize: process.env.NODE_ENV == "DEV",
+      autoLoadEntities: true,
+    };
   },
 
   levels: [
     {
       name: "Butterfly",
-      maxScore: 5400
+      maxScore: 5400,
     },
     {
       name: "Test",
-      maxScore: 99999
-    }
-  ]
-}
+      maxScore: 99999,
+    },
+  ],
+};
 
 export default config;
