0360f79340e2260ca1a15db3fbcff470c2ea2f81
Dmitry221060 <ya.dima42103ya@yandex.ru>
2025-09-05 11:29:35 +0500

    Setup tests

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,43 +3,64 @@ import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended"
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(
-  {
-    ignores:["node_modules", "dist"],
-  },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
+export default [
+  ...tseslint.config(
+    {
+      ignores: ["node_modules", "dist"],
+    },
+    eslint.configs.recommended,
+    ...tseslint.configs.recommendedTypeChecked,
+    eslintPluginPrettierRecommended,
+    {
+      languageOptions: {
+        globals: {
+          ...globals.node,
+          ...globals.jest,
+        },
+        sourceType: "script",
+        parserOptions: {
+          projectService: true,
+          tsconfigRootDir: import.meta.dirname,
+        },
       },
-      sourceType: "script",
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
+      rules: {
+        "node/file-extension-in-import": "off",
+        "class-methods-use-this": "off",
+        "import/no-named-as-default-member": "off",
+        "import/default": "off",
+        "@typescript-eslint/no-floating-promises": "warn",
+        "@typescript-eslint/no-unsafe-argument": "warn",
+
+        "@typescript-eslint/naming-convention": [
+          "warn",
+          {
+            selector: "interface",
+            format: ["PascalCase"],
+            prefix: [],
+          },
+          {
+            selector: "typeAlias",
+            format: ["PascalCase"],
+            prefix: [],
+          },
+        ],
+
+        "object-shorthand": ["warn", "properties"],
       },
     },
+  ),
+  {
+    files: [
+      "**/*.spec.ts",
+      "**/test/integration/**",
+      "**/test/utils/**",
+    ],
     rules: {
-      "node/file-extension-in-import": "off",
-      "class-methods-use-this": "off",
-      "import/no-named-as-default-member": "off",
-      "import/default": "off",
-      "@typescript-eslint/no-floating-promises": "warn",
-      "@typescript-eslint/no-unsafe-argument": "warn",
-
-      "@typescript-eslint/naming-convention": ["warn", {
-        selector: "interface",
-        format: ["PascalCase"],
-        prefix: [],
-      }, {
-        selector: "typeAlias",
-        format: ["PascalCase"],
-        prefix: [],
-      }],
-
-      "object-shorthand": ["warn", "properties"],
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unsafe-assignment": "off",
+      "@typescript-eslint/no-unsafe-call": "off",
+      "@typescript-eslint/no-unsafe-member-access": "off",
+      "@typescript-eslint/require-await": "off",
     },
-});
+  },
+];
