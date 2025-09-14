ed1b8cfba8506169483b4aa6432f9690fffecc84
Dmitry221060 <lazarius2022@gmail.com>
2025-09-14 18:19:44 +0500

    Deprecate old entity creation

--- src/score/score.controller.ts
+++ src/score/score.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Ip, Put } from "@nestjs/common";
+import { Body, Controller, Ip, Put, Post, Header } from "@nestjs/common";
 import { ApiOperation } from "@nestjs/swagger";
 import { ScoreService } from "./score.service";
 import { CreatePlayerScoreDto } from "./dto/create-score.dto";
@@ -10,11 +10,24 @@ export class ScoreController {
   constructor(private readonly scoreService: ScoreService) {}
 
   @Put()
-  async create(@Ip() user: string, @Body() dto: CreatePlayerScoreDto) {
+  @ApiOperation({ summary: "PUT without resource ID is deprecated, use POST instead", deprecated: true })
+  @Header("Deprecated", "true")
+  async oldCreate(@Ip() user: string, @Body() dto: CreatePlayerScoreDto) {
     logger.debug("> PUT /score", dto);
 
     await this.scoreService.addScoreRecord(user, dto);
 
     logger.debug("< PUT /score");
+  }
+
+  @Post()
+  @ApiOperation({ summary: "Create new score record" })
+  async create(@Ip() user: string, @Body() dto: CreatePlayerScoreDto): Promise<PublicPlayerScoreEntity> {
+    logger.debug("> POST /score", dto);
+
+    const response = await this.scoreService.addScoreRecord(user, dto);
+
+    logger.debug("< POST /score");
+    return response;
   }
 }
--- test/integration/score-controller.spec.ts
+++ test/integration/score-controller.spec.ts
@@ -24,21 +24,27 @@ describe("Score", () => {
     await datasource.synchronize(true);
   });
 
-  it("returns 200 when successful", () => {
-    return request(server)
-      .put("/score")
+  it("returns 201 when successful", async () => {
+    const record = { level: testLevel.name, score: 0 };
+    const response = await request(server)
+      .post("/score")
       .set("Content-Type", "application/json")
-      .send({ level: testLevel.name, score: 0 })
-      .expect(200);
+      .send(record)
+      .expect(201);
+    const createdRecord = response.body as PublicPlayerScoreEntity;
+
+    expect(createdRecord).not.toBeNull();
+    expect(createdRecord?.level).toBe(record.level);
+    expect(createdRecord?.score).toBe(record.score);
   });
 
   it("updates database when successful", async () => {
     const record = { level: testLevel.name, score: 1234 };
     await request(server)
-      .put("/score")
+      .post("/score")
       .set("Content-Type", "application/json")
       .send(record)
-      .expect(200);
+      .expect(201);
 
     const databaseRecord = await scoreRepository.findOne({
       where: { ...record },
@@ -51,7 +57,7 @@ describe("Score", () => {
   describe("returns 400 when", () => {
     it("level name is missing", () => {
       return request(server)
-        .put("/score")
+        .post("/score")
         .set("Content-Type", "application/json")
         .send({ score: 0 })
         .expect(400);
@@ -59,7 +65,7 @@ describe("Score", () => {
 
     it("level name format is invalid", () => {
       return request(server)
-        .put("/score")
+        .post("/score")
         .set("Content-Type", "application/json")
         .send({ level: [123, null, "not a plain string"], score: 0 })
         .expect(400);
@@ -67,7 +73,7 @@ describe("Score", () => {
 
     it("level name is empty string", () => {
       return request(server)
-        .put("/score")
+        .post("/score")
         .set("Content-Type", "application/json")
         .send({ level: "", score: 0 })
         .expect(400);
@@ -75,7 +81,7 @@ describe("Score", () => {
 
     it("level name is unknown", () => {
       return request(server)
-        .put("/score")
+        .post("/score")
         .set("Content-Type", "application/json")
         .send({ level: "levelThatDoesNotExist", score: 0 })
         .expect(400);
@@ -83,7 +89,7 @@ describe("Score", () => {
 
     it("score is missing", () => {
       return request(server)
-        .put("/score")
+        .post("/score")
         .set("Content-Type", "application/json")
         .send({ level: testLevel.name })
         .expect(400);
@@ -91,7 +97,7 @@ describe("Score", () => {
 
     it("score format is invalid", () => {
       return request(server)
-        .put("/score")
+        .post("/score")
         .set("Content-Type", "application/json")
         .send({ level: testLevel.name, score: "a lot" })
         .expect(400);
@@ -99,7 +105,7 @@ describe("Score", () => {
 
     it("score exceeds max possible score", () => {
       return request(server)
-        .put("/score")
+        .post("/score")
         .set("Content-Type", "application/json")
         .send({ level: testLevel.name, score: testLevel.maxScore + 1 })
         .expect(400);
@@ -107,7 +113,7 @@ describe("Score", () => {
 
     it("score is negative", () => {
       return request(server)
-        .put("/score")
+        .post("/score")
         .set("Content-Type", "application/json")
         .send({ level: testLevel.name, score: -1 })
         .expect(400);
@@ -115,7 +121,7 @@ describe("Score", () => {
 
     it("score is not an integer", () => {
       return request(server)
-        .put("/score")
+        .post("/score")
         .set("Content-Type", "application/json")
         .send({ level: testLevel.name, score: 11.111 })
         .expect(400);
