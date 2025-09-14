e022a80b5adbf03b75844bfbe787d0a117a57fbc
Dmitry221060 <lazarius2022@gmail.com>
2025-09-14 16:37:03 +0500

    Add helmet

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import { NestFactory } from "@nestjs/core";
 import { ValidationPipe } from "@nestjs/common";
+import helmet from "helmet";
 import config from "./config";
 import logger from "./utils/logger";
 import { AppModule } from "./app.module";
 
 const { port } = config.server;
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  app.use(helmet())
   await app.listen(port);
   logger.info(`Server running at port ${port}`);
 }
